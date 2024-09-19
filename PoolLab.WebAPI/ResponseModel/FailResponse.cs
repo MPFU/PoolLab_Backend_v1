417f32aead1dff6c51a035bd7db77b6fadd8fba3
MinhPhuc <phucnvm2210@gmail.com>
2024-09-19 21:24:27 +0700

    add response model
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

---
+++ PoolLab.WebAPI/ResponseModel/FailResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PoolLab.WebAPI.ResponseModel
+{
+    public class FailResponse
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public object Errors { get; set; }
+    }
+}
