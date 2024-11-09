40a9e28b429542226db2087df949247850dbc9ff
MinhPhuc <phucnvm2210@gmail.com>
2024-11-09 19:40:10 +0700

    fix api and add area img
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

---
+++ PoolLab.Infrastructure/Migrations/20241109123907_AddAreaImg.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoolLab.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAreaImg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AreaImg",
+                table: "Area",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AreaImg",
+                table: "Area");
+        }
+    }
+}
--- PoolLab.Infrastructure/Repository/AreaRepo.cs
+++ PoolLab.Infrastructure/Repository/AreaRepo.cs
@@ -15,6 +15,11 @@ namespace PoolLab.Infrastructure.Interface
         {
         }
 
+        public async Task<bool> CheckDuplicate(Guid storeId, string name)
+        {
+            return await _dbContext.Areas.AnyAsync(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase) && x.StoreId == storeId);
+        }
+
         public async Task<Guid?> GetAreaIdByName(string? name)
         {
             return await _dbContext.Areas.Where(d => d.Name.Equals(name,StringComparison.OrdinalIgnoreCase)).Select(x => x.Id).FirstOrDefaultAsync();
