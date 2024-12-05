ff47ead80e9d554f1a3a43c254bab43beba3b310
MinhPhuc <phucnvm2210@gmail.com>
2024-12-05 15:50:16 +0700

    fix orderdetail
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

--- PoolLab.Infrastructure/PoolLabDbv1Context.cs
+++ PoolLab.Infrastructure/PoolLabDbv1Context.cs
@@ -87,7 +87,7 @@ public partial class PoolLabDbv1Context : DbContext
             .AddJsonFile("appsettings.json", true, true)
             .Build();
 
-        connectionString = config.GetConnectionString("AzureDB");
+        connectionString = config.GetConnectionString("PhucDatabase");
         return connectionString;
     }
 
